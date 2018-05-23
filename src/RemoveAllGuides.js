aa08f36505b5094f8fb1651571b774f8e28a10a2
ichik <ichikumer@gmail.com>
2018-05-23 13:39:24 +0200

    Migration!

---
+++ src/RemoveAllGuides.js
@@ -0,0 +1,22 @@
+export default function(context) {
+	var selection = context.selection;
+	if(selection.count() > 0){
+		for(var i = 0; i < selection.count(); i++){
+			if(selection[i].class() == "MSArtboardGroup"){
+				var artboard = selection[i];
+				var horizontalGuideCount = artboard.verticalRulerData().numberOfGuides();
+				var verticalGuideCount = artboard.horizontalRulerData().numberOfGuides();
+				while (horizontalGuideCount > 0) {
+					artboard.verticalRulerData().removeGuideAtIndex(0);
+					horizontalGuideCount = artboard.verticalRulerData().numberOfGuides();
+				};
+				while (verticalGuideCount > 0) {
+					artboard.horizontalRulerData().removeGuideAtIndex(0);
+					verticalGuideCount = artboard.horizontalRulerData().numberOfGuides();
+				};
+			};
+		};
+	} else {
+    context.document.showMessage('Select some artboards to 💨 guides from');
+  }
+};
